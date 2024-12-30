4e34bbcec95ae4c3d16632aa3c666894f5c6f940
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-12-31 00:50:53 +0530

    added bootstrap in ejs

--- index.js
+++ index.js
@@ -7,4 +7,8 @@ const PORT = 8000;
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.get("/", (req, res) => {
+  res.render("Home");
+});
+
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
